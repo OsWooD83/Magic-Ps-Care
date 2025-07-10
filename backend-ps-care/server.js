5d506f0d9e9c36ef6d99e8e2e1595216ecbf0d94
OsWooD83 <enzovercellotti@hotmail.com>
2025-07-10 22:23:25 +0200

    FINAL CORS FIX: Explicit origin header handling

--- backend-ps-care/server.js
+++ backend-ps-care/server.js
@@ -24,7 +24,12 @@ app.options('*', cors());
 
 // SOLUTION EMERGENCY : Headers CORS manuels pour forcer la propagation
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', req.headers.origin || '*');
+  const origin = req.headers.origin;
+  if (origin) {
+    res.header('Access-Control-Allow-Origin', origin);
+  } else {
+    res.header('Access-Control-Allow-Origin', '*');
+  }
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With, Accept, Origin');
