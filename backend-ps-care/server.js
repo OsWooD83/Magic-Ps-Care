5d9d7d1bae7f9e9940630ad863b7e6969a550edc
OsWooD83 <enzovercellotti@hotmail.com>
2025-07-10 16:44:58 +0200

    Correction CORS: ajout de la virgule manquante dans la liste des origines

--- backend-ps-care/server.js
+++ backend-ps-care/server.js
@@ -14,7 +14,7 @@ app.use(cors({
   origin: [
     'https://magicpscare.vercel.app',
     'https://association-magic-ps-care-cogf6ko31.vercel.app',
-    'https://association-magic-ps-care-q76uuhra0.vercel.app'
+    'https://association-magic-ps-care-q76uuhra0.vercel.app',
     'https://backend-ps-care.onrender.com'
   ],
   credentials: true
