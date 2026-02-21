3cffbb8f57b4613405f764e49f1802d27b2bb04d
OsWooD83 <enzovercellotti@hotmail.com>
2026-02-21 20:42:12 +0100

    update html

--- app.js
+++ app.js
@@ -1,4 +1,9 @@
+
+import express from 'express';
 import cors from 'cors';
+
+const app = express();
+
 app.use(cors({
   origin: [
     'https://oswood83.github.io'
@@ -6,7 +11,12 @@ app.use(cors({
   credentials: true // si tu utilises les cookies
 }));
 
+app.use(express.json());
+
 app.post('/api/logout', (req, res) => {
   res.status(200).json({ message: 'Déconnecté' });
 });
+
+// Pour que ce fichier puisse être utilisé comme module ou serveur principal
+export default app;
 
